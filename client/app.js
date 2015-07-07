b58d975d04b0be0e91cd7ceb0a542dad6fde22ee
Michael Roberts <m_roberts6@u.pacific.edu>
2015-07-07 09:47:03 -0700

    get jquery to prevent form submit default

--- client/app.js
+++ client/app.js
@@ -10,4 +10,15 @@
 // do AJAX request
 
 
+$(document).ready(function(){
+  /*====== get multiple forms working after one form======*/;
+  // $("form").each(function(i, f) {
+  //   // code here
+  // });
 
+  $("form").submit(function(e, f) {
+    e.preventDefault();
+    console.log('submitted');
+  });
+
+});
